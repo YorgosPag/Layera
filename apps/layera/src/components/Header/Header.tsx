d576838a5b2ae9e2465cf31e0243821f7134e207
YorgosPag <georgios.pagonis@gmail.com>
2025-11-12 17:22:19 +0200

    🎯 Header Implementation - SAFETY    CHECKPOINT

--- apps/layera/src/components/Header/Header.tsx
+++ apps/layera/src/components/Header/Header.tsx
@@ -10,57 +10,20 @@
 
 import React from 'react';
 import { Box } from '@layera/layout';
-import { PlusIcon, LocationIcon, MenuIcon, UserIcon, SettingsIcon, SearchIcon, BuildingIcon, BriefcaseIcon } from '@layera/icons';
-import { Modal, ModalHeader, ModalContent, useModal } from '@layera/modals';
+import { PlusIcon, LocationIcon, MenuIcon, UserIcon, SettingsIcon, SearchIcon } from '@layera/icons';
 import { headerStyles } from './Header.styles';
 
-export const Header: React.FC = () => {
-  const { isOpen, open, close } = useModal();
+interface HeaderProps {
+  onAddContentClick?: () => void;
+}
+
+export const Header: React.FC<HeaderProps> = ({ onAddContentClick }) => {
 
   return (
-    <>
-      <Modal
-        open={isOpen}
-        onClose={close}
-        size="md"
-        aria-labelledby="add-content-title"
-      >
-        <ModalHeader title="Προσθήκη νέου περιεχομένου" />
-        <ModalContent>
-          <Box style={headerStyles.paddingLarge}>
-            <Box style={{...headerStyles.gridContainer, ...headerStyles.gridTablet}}>
-              <Box style={headerStyles.cardClickable}>
-                <Box style={{...headerStyles.flexContainer, gap: 'var(--layera-global-spacing-2)', ...headerStyles.marginBottomSmall}}>
-                  <BuildingIcon size="lg" style={headerStyles.blueIcon} />
-                  <Box style={headerStyles.typographyLarge}>
-                    Ακίνητα
-                  </Box>
-                </Box>
-                <Box style={headerStyles.typographySmall}>
-                  Προσθήκη νέου ακινήτου στην πλατφόρμα
-                </Box>
-              </Box>
-
-              <Box style={headerStyles.cardClickable}>
-                <Box style={{...headerStyles.flexContainer, gap: 'var(--layera-global-spacing-2)', ...headerStyles.marginBottomSmall}}>
-                  <BriefcaseIcon size="lg" style={headerStyles.greenIcon} />
-                  <Box style={headerStyles.typographyLarge}>
-                    Εργασία
-                  </Box>
-                </Box>
-                <Box style={headerStyles.typographySmall}>
-                  Προσθήκη νέας αγγελίας εργασίας
-                </Box>
-              </Box>
-            </Box>
-          </Box>
-        </ModalContent>
-      </Modal>
-
-      <Box style={headerStyles.container}>
+    <Box style={headerStyles.container}>
         {/* Αριστερά: Πλήκτρο με + και κείμενο Geo-Canvas */}
         <Box style={headerStyles.flexContainer}>
-          <button onClick={open} style={headerStyles.actionButton}>
+          <button onClick={onAddContentClick} style={headerStyles.actionButton}>
             <PlusIcon size="sm" style={headerStyles.whiteIcon} />
           </button>
           <span style={headerStyles.headerTitle}>Geo-Canvas</span>
